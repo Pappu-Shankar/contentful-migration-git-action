3c9c48854e9ffb60841fc413932dd062d1bd35ff
Pappu Shankar <Pappu.Shankar@redgrapebs.com>
2022-11-16 04:08:44 +0530

    adding the updated yml

--- test-migrations/2.js
+++ test-migrations/2.js
@@ -1,42 +1,44 @@
+module.exports = function () {};
+
 // @ts-check
 /** @type import('contentful-migration').MigrationFunction */
-module.exports = function (migration) {
-	const navItem = migration
-		.createContentType('navItem')
-		.name('NavItem')
-		.displayField('name');
+// module.exports = function (migration) {
+// 	const navItem = migration
+// 		.createContentType('navItem')
+// 		.name('NavItem')
+// 		.displayField('name');
 
-	navItem.createField('name').type('Symbol').name('Name').required(true);
-	navItem.createField('url').type('Symbol').name('url');
+// 	navItem.createField('name').type('Symbol').name('Name').required(true);
+// 	navItem.createField('url').type('Symbol').name('url');
 
-	navItem.createField('childrens', {
-		name: 'Childrens',
-		type: 'Array',
-		items: {
-			type: 'Link',
-			validations: [],
-			linkType: 'Entry',
-		},
-	});
+// 	navItem.createField('childrens', {
+// 		name: 'Childrens',
+// 		type: 'Array',
+// 		items: {
+// 			type: 'Link',
+// 			validations: [],
+// 			linkType: 'Entry',
+// 		},
+// 	});
 
-	const navHeader = migration
-		.createContentType('navigationHeader')
-		.name('NavHeader')
-		.displayField('title');
+// 	const navHeader = migration
+// 		.createContentType('navigationHeader')
+// 		.name('NavHeader')
+// 		.displayField('title');
 
-	navHeader.createField('title').type('Symbol').name('Title').required(true);
-	navHeader.createField('logo').type('Link').name('Logo').linkType('Asset');
-	navHeader.createField('navItems', {
-		name: 'NavItems',
-		type: 'Array',
-		items: {
-			type: 'Link',
-			validations: [],
-			linkType: 'Entry',
-		},
-	});
-	navHeader.createField('allowShoppingCart').type('Boolean').name('AllowShoppingCart');
-	navHeader.createField('allowSearch').type('Boolean').name('AllowSearch');
+// 	navHeader.createField('title').type('Symbol').name('Title').required(true);
+// 	navHeader.createField('logo').type('Link').name('Logo').linkType('Asset');
+// 	navHeader.createField('navItems', {
+// 		name: 'NavItems',
+// 		type: 'Array',
+// 		items: {
+// 			type: 'Link',
+// 			validations: [],
+// 			linkType: 'Entry',
+// 		},
+// 	});
+// 	navHeader.createField('allowShoppingCart').type('Boolean').name('AllowShoppingCart');
+// 	navHeader.createField('allowSearch').type('Boolean').name('AllowSearch');
 
 	
-};
+// };
